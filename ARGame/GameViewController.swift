c949640d8b34f75bea25255d52b4e99f4aa01bed
Punyugi2012 <58160216@go.buu.ac.th>
2018-08-23 19:21:07 +0700

    added box and added auto light

--- ARGame/GameViewController.swift
+++ ARGame/GameViewController.swift
@@ -19,10 +19,20 @@ class GameViewController: UIViewController {
         let configuration = ARWorldTrackingConfiguration()
         myARView.session.run(configuration)
         myARView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        myARView.autoenablesDefaultLighting = true
     }
 
     @IBAction func tappedPlusBtn(_ sender: UIButton) {
+        addBox()
     }
     
+    func addBox() {
+        let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.0)
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+        boxNode.position = SCNVector3(0, 0, 0)
+        myARView.scene.rootNode.addChildNode(boxNode)
+    }
 
 }
